12c39cc342d4aea408564c874210266408c2f00d
Daniil Okonskiy <dofeternity@gmail.com>
2022-11-12 22:39:25 +0600

    Added if tag helper (like in angular lmao)

---
+++ RecipeApplication.WebHost/CustomTagHelpers/IfTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace RecipeApplication.CustomTagHelpers;
+
+[HtmlTargetElement(Attributes = "if")]
+public class IfTagHelper : TagHelper
+{
+    [HtmlAttributeName("if")] public bool RenderContent { get; set; } = true;
+    
+    public override int Order => int.MinValue;
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if (!RenderContent)
+        {
+            output.TagName = null;
+            output.SuppressOutput();
+        }
+    }
+}
