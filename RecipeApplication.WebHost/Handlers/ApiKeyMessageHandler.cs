cbc6c2eda24bfcd3a79e166900ffb6928fb86a98
Daniil Okonskiy <dofeternity@gmail.com>
2022-11-13 23:16:58 +0600

    Added custom Http Message Handler for wikipedia client

---
+++ RecipeApplication.WebHost/Handlers/ApiKeyMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace RecipeApplication.Handlers;
+
+public class ApiKeyMessageHandler : DelegatingHandler
+{
+    private readonly IConfiguration _configuration;
+
+    public ApiKeyMessageHandler(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.Headers.Add("X-API-KEY", _configuration.GetValue<string>("ApiKey"));
+        
+        var response = await base.SendAsync(request, cancellationToken);
+
+        return response;
+    }
+}
--- RecipeApplication.WebHost/Program.cs
+++ RecipeApplication.WebHost/Program.cs
@@ -8,6 +8,7 @@ using RecipeApplication.DataAccess;
 using RecipeApplication.DataAccess.Repositories;
 using RecipeApplication.DataAccess.Repositories.Sql;
 using RecipeApplication.Extensions;
+using RecipeApplication.Handlers;
 using RecipeApplication.Policies;
 using RecipeApplication.Services;
 using RecipeApplication.SyncDataServices;
@@ -18,7 +19,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Configure application
 var configuration = builder.Configuration;
 
-configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true,
+    reloadOnChange: true);
 if (builder.Environment.IsDevelopment())
 {
     configuration.AddUserSecrets<Program>(optional: true, reloadOnChange: true);
@@ -38,27 +40,24 @@ services.AddDbContext<AppDbContext>(options =>
 
     options.UseSqlServer(connection);
 });
-services.AddDefaultIdentity<ApplicationUser>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = true;
-}).AddEntityFrameworkStores<AppDbContext>();
-
+services.AddDefaultIdentity<ApplicationUser>(options => { options.SignIn.RequireConfirmedAccount = true; })
+    .AddEntityFrameworkStores<AppDbContext>();
 services.AddScoped<IRecipeRepository, RecipeRepository>();
 services.AddScoped<IRepository<Recipe>, SqlRepository<Recipe>>();
 services.AddScoped<IRecipesService, RecipesService>();
 services.AddScoped<IAuthorizationHandler, IsRecipeOwnerHandler>();
 services.AddAuthorization(options =>
 {
-    options.AddPolicy("IsRecipeOwner", policyBuilder =>
-    {
-        policyBuilder.AddRequirements(new IsRecipeOwnerRequirement());
-    });
+    options.AddPolicy("IsRecipeOwner",
+        policyBuilder => { policyBuilder.AddRequirements(new IsRecipeOwnerRequirement()); });
 });
 services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+services.AddTransient<ApiKeyMessageHandler>();
 services.AddHttpClient<IWikipediaClient, WikipediaClient>(client =>
-{
-    client.BaseAddress = new Uri(configuration.GetValue<string>("WikipediaApiAddress")!);
-});
+    {
+        client.BaseAddress = new Uri(configuration.GetValue<string>("WikipediaApiAddress")!);
+    })
+    .AddHttpMessageHandler<ApiKeyMessageHandler>();
 services.AddSingleton<IClientPolicy, ClientPolicy>();
 
 // Use services
